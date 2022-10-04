dd83c075689e46f75c640ca5abec469aa740322b
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2022-10-04 18:50:18 +0530

    import java.util.*; class Area {     public static void main(String[] args) {         Scanner in = new Scanner(System.in);         char c = in.next().charAt(0);         int asci=c;         System.out.print(asci);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Area {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); 
+        char c = in.next().charAt(0);
+        int asci=c;
+        System.out.print(asci);
+    }
+}
