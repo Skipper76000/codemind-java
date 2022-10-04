12392d1fbcb4f036e2a748483f6e73083b9402c1
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2022-10-04 17:52:10 +0530

    import java.util.*; class Area {     public static void main(String[] args) {         Scanner in = new Scanner(System.in);         int a = in.nextInt();         int b = in.nextInt();         int ans = a/b;         System.out.print(ans);     } }

---
+++ Division.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Area {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int ans = a/b;
+        System.out.print(ans);
+    }
+}
