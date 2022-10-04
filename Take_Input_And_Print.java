009c46dcd142e737fbfa1da9ce9b5ec27614758b
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2022-10-04 18:09:30 +0530

    import java.util.*; class Area {     public static void main(String[] args) {         Scanner in = new Scanner(System.in);         String ans=in.nextLine();         System.out.print(ans);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Area {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String ans=in.nextLine();
+        System.out.print(ans);
+    }
+}
